4d5c97d73e3c0871be66960cbaab891d76dcef14
SubhaNagaraj <nagarajsubha688@gmail.com>
2022-01-11 16:47:17 +0530

    test commit5

--- sam.sh
+++ sam.sh
@@ -1,4 +1,5 @@
 #! /bin/bash 
+run=$(date +%F-%T)
 
 sudo cp -r /var/lib/jenkins/workspace/SpringBootProject1/employee/target/employee-0.0.1-SNAPSHOT.war /opt/scrunch/backup/$run 
 sudo rm -rf /opt/scrunch/deploy/$run 
