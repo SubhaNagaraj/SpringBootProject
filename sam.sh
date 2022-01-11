b43362278807a2fc3dc9db1bae8eccf2adf00d65
SubhaNagaraj <nagarajsubha688@gmail.com>
2022-01-11 16:22:27 +0530

    test commit4

--- sam.sh
+++ sam.sh
@@ -1,5 +1,5 @@
 #! /bin/bash 
 
 sudo cp -r /var/lib/jenkins/workspace/SpringBootProject1/employee/target/employee-0.0.1-SNAPSHOT.war /opt/scrunch/backup/$run 
-sudo rm -rf /opt/scrunch/deploy/$run  
-nohup java -jar /opt/scrunch/deploy/$run /var/lib/jenkins/workspace/SpringBootProject1/employee/target/employee-0.0.1-SNAPSHOT.war &
+sudo rm -rf /opt/scrunch/deploy/$run 
+nohup java -jar /var/lib/jenkins/workspace/SpringBootProject1/employee/target/employee-0.0.1-SNAPSHOT.war &
