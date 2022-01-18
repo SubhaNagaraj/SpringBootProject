70920ad507d25763ed1dba9d00f0439e2b4afe56
SubhaNagaraj <96992243+SubhaNagaraj@users.noreply.github.com>
2022-01-18 16:14:43 +0530

    Update EmployeeController.java
    example test commit

--- employee/src/main/java/com/springbt/employee/Controller/EmployeeController.java
+++ employee/src/main/java/com/springbt/employee/Controller/EmployeeController.java
@@ -22,7 +22,7 @@ public class EmployeeController {
 	@Autowired
 	private EmpolyeeService empolyeeService;
 
-	@GetMapping("/al")
+	@GetMapping("/all")
 	public List<EmployeeModel> all() {
 		return empolyeeService.findAllrecords();
 	}
