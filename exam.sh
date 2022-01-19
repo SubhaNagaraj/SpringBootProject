888c8b36d9899e2cbeae918b592f9f6d8767da65
SubhaNagaraj <nagarajsubha688@gmail.com>
2022-01-19 16:18:22 +0530

    commit 3

--- exam.sh
+++ exam.sh
@@ -5,12 +5,12 @@ TIMESTAMP_FOLDER=$(date +%F-%T)
 PROJECT_DIR=/myagent/_work/2/s
 
 sudo rm -rf /myagent/scrunwork/deploy/
-sudo mkdir -p /myagent/scrunwork/deploy/$TIMESTAMP_FOLDER
+sudo mkdir -p /myagent/scrunwork/deploy/
 sudo mkdir -p /myagent/scrunwork/backup/$TIMESTAMP_FOLDER
 
 
 sudo cp -r $PROJECT_DIR/employee/target/employee-0.0.1-SNAPSHOT.war  /myagent/scrunwork/backup/$TIMESTAMP_FOLDER/
-sudo cp -r $PROJECT_DIR/employee/target/employee-0.0.1-SNAPSHOT.war  /myagent/scrunwork/deploy/$TIMESTAMP_FOLDER/
+sudo cp -r $PROJECT_DIR/employee/target/employee-0.0.1-SNAPSHOT.war  /myagent/scrunwork/deploy/
 
 sudo kill -9 $(sudo lsof -t -i:8081)
 nohup java -jar /myagent/scrunwork/deploy/employee-0.0.1-SNAPSHOT.war & 
