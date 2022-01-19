f6ab66cb888e1c893d28243cd453027b93e600af
SubhaNagaraj <nagarajsubha688@gmail.com>
2022-01-19 17:06:18 +0530

    commit9

--- exam.sh
+++ exam.sh
@@ -12,5 +12,5 @@ sudo mkdir -p /myagent/scrunwork/backup/$TIMESTAMP_FOLDER
 sudo cp -r $PROJECT_DIR/employee/target/employee-0.0.1-SNAPSHOT.war  /myagent/scrunwork/backup/$TIMESTAMP_FOLDER/
 sudo cp -r $PROJECT_DIR/employee/target/employee-0.0.1-SNAPSHOT.war  /myagent/scrunwork/deploy/
 
-sudo kill -1 $(sudo lsof -t -i:8081)
+sudo kill -9 $(sudo lsof -t -i:8081)
 nohup java -jar /myagent/scrunwork/deploy/employee-0.0.1-SNAPSHOT.war & 
