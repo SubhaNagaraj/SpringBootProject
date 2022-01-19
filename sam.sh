a3b0d45b279c9ccee11b2a64f6117d9e47116712
SubhaNagaraj <nagarajsubha688@gmail.com>
2022-01-19 10:07:19 +0530

    test git commit

--- sam.sh
+++ sam.sh
@@ -1,6 +1,16 @@
 #! /bin/bash 
-run=$(date +%F-%T)
 
-sudo cp -r /var/lib/jenkins/workspace/SpringBootProject1/employee/target/employee-0.0.1-SNAPSHOT.war /opt/scrunch/backup/$run 
-sudo rm -rf /opt/scrunch/deploy/$run 
-nohup java -jar /var/lib/jenkins/workspace/SpringBootProject1/employee/target/employee-0.0.1-SNAPSHOT.war &
+
+TIMESTAMP_FOLDER=$(date +%F-%T)
+PROJECT_DIR=/var/lib/jenkins/workspace/SpringBootProject1
+
+sudo rm -rf /opt/scrunch/deploy/
+sudo mkdir -p /opt/scrunch/deploy/
+sudo mkdir -p /opt/scrunch/backup/$TIMESTAMP_FOLDER
+
+
+sudo cp -r $PROJECT_DIR/employee/target/employee-0.0.1-SNAPSHOT.war  /opt/scrunch/backup/$TIMESTAMP_FOLDER/
+sudo cp -r $PROJECT_DIR/employee/target/employee-0.0.1-SNAPSHOT.war /opt/scrunch/deploy/
+
+sudo kill -9 $(sudo lsof -t -i:8081)
+nohup java -jar /opt/scrunch/deploy/employee-0.0.1-SNAPSHOT.war & 
