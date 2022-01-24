1f8e8711e3a2a6fa835e879e370a80ba6f2e70a4
SubhaNagaraj <nagarajsubha688@gmail.com>
2022-01-24 16:21:55 +0530

    deploy commit 1

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ PROJECT_DIR=/myagent/_work/2/s/
 
 cd $PROJECT_DIR/employee/
 
-docker build -t subha6867/employee:latest .
+sudo docker build -t subha6867/employee:latest .
 
-docker push subha6867/employee:latest
+sudo docker push subha6867/employee:latest
 
 kubectl delete all --all 
 
